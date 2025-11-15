75708f2bd6e4d54378646a39b5d43b1a9e50fea0
apescasio <pescasioaaron92@gmail.com>
2025-11-15 19:58:31 +0100

    PCL7C - Updated Languages with S.A.F.E + Socials Links

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,27 +1,47 @@
-import 'fumadocs-ui/style.css';
-import { RootProvider } from 'fumadocs-ui/provider/next';
-import { Inter } from 'next/font/google';
-import { defineI18nUI } from 'fumadocs-ui/i18n';
-import { i18n } from '@/lib/i18n';
+import "fumadocs-ui/style.css";
+import { RootProvider } from "fumadocs-ui/provider/next";
+import { Inter } from "next/font/google";
+import { defineI18nUI } from "fumadocs-ui/i18n";
+import { i18n } from "@/lib/i18n";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
 const { provider } = defineI18nUI(i18n, {
   translations: {
-    en: {
-      displayName: 'English',
+    es: {
+      displayName: "Spanish",
+      toc: "En esta página",
+      search: "Buscar",
+      lastUpdate: "Última actualización",
+      searchNoResult: "Esta página no tiene resultados",
+      previousPage: "Página anterior",
+      nextPage: "Página siguiente",
+      chooseLanguage: "Elegir lengua",
     },
-    cn: {
-      displayName: 'Chinese',
-      toc: '目錄',
-      search: '搜尋文檔',
-      lastUpdate: '最後更新於',
-      searchNoResult: '沒有結果',
-      previousPage: '上一頁',
-      nextPage: '下一頁',
-      chooseLanguage: '選擇語言',
+    ar: {
+      displayName: "Arabic",
+      toc: "جدول المحتويات",
+      search: "بحث",
+      lastUpdate: "آخر تحديث",
+      searchNoResult: "لا توجد نتائج في هذه الصفحة",
+      previousPage: "الصفحة السابقة",
+      nextPage: "الصفحة التالية",
+      chooseLanguage: "اختر اللغة",
+    },
+    fr: {
+      displayName: "French",
+      toc: "Table des matières",
+      search: "Chercher",
+      lastUpdate: "Dernière mise à jour",
+      searchNoResult: "Cette page n'a pas de résultats",
+      previousPage: "Page précédente",
+      nextPage: "Page suivante",
+      chooseLanguage: "Choisir la langue",
+    },
+    en: {
+      displayName: "English",
     },
   },
 });
@@ -29,17 +49,15 @@ const { provider } = defineI18nUI(i18n, {
 export default async function Layout({
   params,
   children,
-}: LayoutProps<'/[lang]'>) {
-  const { lang } = await params;
+}: {
+  params: Promise<{ lang: string }>;
+  children: React.ReactNode;
+}) {
+  const lang = (await params).lang;
+
   return (
     <html lang={lang} className={inter.className} suppressHydrationWarning>
-      <body
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          minHeight: '100vh',
-        }}
-      >
+      <body className="flex flex-col min-h-screen">
         <RootProvider i18n={provider(lang)}>{children}</RootProvider>
       </body>
     </html>
--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,19 +1,79 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import { i18n } from '@/lib/i18n';
+import { i18n } from "@/lib/i18n";
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+
+import IconLinkedin from "@/components/icons/linkedin";
+import IconYouTubeV2 from "@/components/icons/youtube";
+import IconInstagram from "@/components/icons/instagram";
+import IconGitHub from "@/components/icons/github";
+import IconMemov2 from "@/components/icons/memov2";
+import Link from "next/link";
 
 export function baseOptions(locale: string): BaseLayoutProps {
   return {
     i18n,
     nav: {
-      title: locale === 'cn' ? 'Chinese Docs' : 'English Docs',
-      url: `/${locale}`,
+      title: (
+        <>
+          <IconMemov2
+            className="size-10 shrink-0"
+            aria-label="Memo Icon"
+            width={44}
+            height={44}
+          />
+          Memo
+        </>
+      ),
+      url: `/${locale}`, // Add this to point to /${lang} // blabla
     },
-    githubUrl: 'https://github.com',
     links: [
       {
-        type: 'main',
-        text: locale === 'cn' ? '文檔' : 'Documentation',
-        url: `/${locale}/docs`,
+        type: "main",
+        text: locale === "en" ? "Aaron (Iso) Pescasio" : "Aaron (Iso) Pescasio",
+        url: "https://apescasio.fr",
+      },
+      {
+        type: "icon",
+        text: "YouTube",
+        url: "https://youtube.com/@apescasio",
+        icon: (
+          <span style={{ transform: "scale(1.3)" }}>
+            <IconYouTubeV2 />
+          </span>
+        ),
+        external: true,
+      },
+      {
+        type: "icon",
+        text: "LinkedIn",
+        url: "https://www.linkedin.com/in/aaron-pescasio",
+        icon: (
+          <span style={{ transform: "scale(1.3)" }}>
+            <IconLinkedin />
+          </span>
+        ),
+        external: true,
+      },
+      {
+        type: "icon",
+        text: "Instagram",
+        url: "https://www.instagram.com/himapescasio",
+        icon: (
+          <span style={{ transform: "scale(1.2)" }}>
+            <IconInstagram />
+          </span>
+        ),
+        external: true,
+      },
+      {
+        type: "icon",
+        text: "GitHub",
+        url: "https://github.com/apescasio",
+        icon: (
+          <span style={{ transform: "scale(1.3)" }}>
+            <IconGitHub />
+          </span>
+        ),
+        external: true,
       },
     ],
   };
